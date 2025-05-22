db0e2cccb0015fcb4f226b9f176c88d68895edcc
Federico Tambara <98750668+ftambara@users.noreply.github.com>
2025-05-22 15:55:43 -0300

    Add wrap ad spell for text files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,22 +1,32 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("my_nvim_" .. name, { clear = true })
+    return vim.api.nvim_create_augroup("my_nvim_" .. name, { clear = true })
 end
 
 -- LSP
 local completion = vim.g.completion_mode or "blink" -- or "native"
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client then
-      -- Built-in completion
-      if completion == "native" and client:supports_method "textDocument/completion" then
-        vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-      end
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then
+            -- Built-in completion
+            if completion == "native" and client:supports_method "textDocument/completion" then
+                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+            end
 
-      -- Inlay hints
-      if client:supports_method "textDocument/inlayHints" then
-        vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-      end
-    end
-  end,
+            -- Inlay hints
+            if client:supports_method "textDocument/inlayHints" then
+                vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+            end
+        end
+    end,
+})
+
+-- Wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup "wrap_spell",
+    pattern = { "*.txt", "*.tex", "*.typ", "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
 })
